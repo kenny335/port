633c1efb0f48ec14517f0e917c59661c5be33de4
Kenny Quach <kq22@scarletmail.rutgers.edu>
2019-05-21 16:20:37 -0400

    Remove background

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -6,12 +6,11 @@ import {Switch} from "react-mdl"
 
 class Landingpage extends React.Component {
 
+
+
     render() {
         return(
         <div>
-            <div class="hero">
-            <h1>Ambitous web developer</h1>
-            </div>
             <img style={{margin:"50px", height:"300px", width:"300px", borderRadius:"50%", border:"2px solid black"}} src="https://media.licdn.com/dms/image/C5103AQHpdHxmwj6c9w/profile-displayphoto-shrink_200_200/0?e=1564012800&v=beta&t=QHwArkHucm5Nc6tzR2qhAPJ2hUcQ1Nt0WM2ShpK5e70"/>
             <div className="content">
             <div className="columnIt">
